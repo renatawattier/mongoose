fc272b65447af61ca1cee5f7e6cdeac982498a14
coxipozina <coxipozina@gmail.com>
2018-09-30 12:54:33 -0700

    ...

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express')
+var CONNECTION_URI = process.env.MONGODB_URI || 'mongodeb://localhost/users';
 var app = express()
 var expressHandlebars = require('express-handlebars')
 var cheerio = require('cheerio')
@@ -7,11 +8,11 @@ var bodyParser = require('body-parser')
 var request = require('request')
 var Article = require('./articleModel')
 
-var port = 3000 || process.env.PORT
+var PORT = 3000 || process.env.PORT
 
 app.use(bodyParser.json())
 
- mongoose.connect('mongodb://newuser:123456A@ds239692.mlab.com:39692/mongo-mongoose')
+ mongoose.connect(CONNECTION_URI, 'mongodb://newuser:123456A@ds239692.mlab.com:39692/mongo-mongoose')
 
 
 app.engine('handlebars', expressHandlebars({ defualtLayout: "main"}))
@@ -60,8 +61,8 @@ app.get("/clear", function(req, res){
 })
 
 
-app.listen(port, function(){
-    console.log("app is listening on port" + port)
+app.listen(PORT, function(){
+    console.log("app is listening on port" + PORT)
 });
 
 
